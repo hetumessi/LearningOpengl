cc377f25ef453f0b390bd9b0ac2f4910c1638ba0
1 <a11@1deMacBook-Pro.local>
2024-06-17 03:28:24 +0800

    init

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/a11/projects/opengl_proj
+# Build directory: /Users/a11/projects/opengl_proj/out/build/opengl_proj
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/a11/projects/opengl_proj/src
+# Build directory: /Users/a11/projects/opengl_proj/out/build/opengl_proj/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
